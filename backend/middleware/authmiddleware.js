c9e9c62e8aecb3873ad73414c4773241aac3a512
fine-grainedmonster <azatitebi@yahoo.com>
2023-09-27 15:59:30 +0100

    login testing and works

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,37 +1,49 @@
 const jwt = require("jsonwebtoken");
-const asynchandler = require("express-async-handler");
-const user = require("../model/users/user");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/users/user");
 
-const protect = asynchandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token;
-  
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    
     try {
-      //get token from header
-      
+      // Get token from header
       token = req.headers.authorization.split(" ")[1];
-     
+
+      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-     
-      
-      //get user from the token;
-      req.auth = await usert.findById(decoded.id).select("-password");
-      
+
+      // Calculate the current time in seconds since Unix epoch
+      const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+
+      // Calculate the time elapsed since token creation
+      const timeElapsedInSeconds = currentTimeInSeconds - decoded.iat;
+
+      // Check if more than 12 hours (12 hours * 60 minutes * 60 seconds)
+      if (timeElapsedInSeconds > 12 * 60 * 60) {
+        // Token has expired
+        res.status(401);
+        throw new Error("Token has expired");
+      }
+
+      // Get user from the token
+      req.auth = await User.findById(decoded.id).select("-password");
+
       next();
     } catch (error) {
-      console.log(error);
+      console.error(error);
       res.status(401);
-      throw new Error("not authorize");
+      throw new Error("Not authorized");
     }
   }
+
   if (!token) {
     res.status(401);
-    throw new Error("not authorized, no token");
+    throw new Error("Not authorized, no token");
   }
 });
-//how to get the stock data of Tesla in javascript?
+
 module.exports = { protect };
