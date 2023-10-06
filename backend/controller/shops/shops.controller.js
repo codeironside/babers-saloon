098c69588143c2ce2daa05d12173a14cf7b6d8df
fine-grainedmonster <azatitebi@yahoo.com>
2023-10-06 22:18:47 +0100

    started work on the shops

--- backend/controller/shops/shops.controller.js
+++ backend/controller/shops/shops.controller.js
@@ -3,6 +3,10 @@ const SHOPS = require("../../model/shops/shop");
 const logger = require("../../utils/logger")
 const USER = require("../../model/users/user.js");
 const working_hours = require('../../model/shops/openinghours.model')
+
+//access privare
+//route /shops/register/
+// route for creating shops
 const create_shops =asynchandler( async (req, res) => {
   try {
     const { id } = req.auth;
@@ -148,4 +152,89 @@ const login_shops=asynchandler(async(req,res)=>{
         throw new Error('no shops found')
     }
 })
+///access private 
+//desc list all shops
+//routes /shops/all
+const getallshops = asyncHandler(async(req,res)=>{
+  const page = parseInt(req.query.poge)||1
+  const pageSize =parseInt(req.query.pageSize)|| 10
+  try{
+    const totalCount = await SHOP.countDocuments()
+    const totalpages = Math.cell(totalCount/pageSize)
+    const shops = await SHOP.find()
+    .skip((page -1) * pageSize)
+    .limit(pageSize)
+    res.json({
+      data:shops,
+      pahe:page,
+      totalPages:totalpages
+    })
+  }
+  catch(error){
+    throw new Error('internal server Error')
+  }
+})
+// desc update shops
+//route /shops/update/
+//access private 
+// Controller function to update a user
+//route /user/updateac
+//access private
+//data updateData
+const updateShops = async (req, res) => {
+  const { userId } = req.params; // Get the user ID from the route parameters
+  const clientIp = req.clientIp;
+  const {_id, ...updateData }= req.body; // Get the updated data from the request body
+
+  try {
+    if(!userId){throw new Error('params is empty')}
+    // Use findByIdAndUpdate to update the user document by ID
+    if(!updateData){
+      throw new Error("body is empty")
+    }
+    if(_id){
+      throw new Error('_id not found')
+    }
+    
+    const updatedShops = await SHOPS.findByIdAndUpdate(_id, updateData, {
+      new: true, // Return the updated user document
+    });
+
+    if (!updatedShops) {
+      throw new Error('Shop not found ')
+    }
+
+    res.status(202).json(updatedShops);
+    const createdAt = updatedShops.updatedAt; // Assuming createdAt is a Date object in your Mongoose schema
+  const watCreatedAt = convertToWAT(createdAt);
+  const location = await getLocation(clientIp);
+    logger.info(
+      `user with id ${userId},updated shop ${id} at ${watCreatedAt} - ${res.statusCode} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}  - from ${location}`
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error('server Error')
+  }
+};
+
+const getLocation = asynchandler(async (ip) => {
+  try {
+    // Set endpoint and your access key
+    const accessKey = process.env.ip_secret_key;
+    const url =
+      "http://apiip.net/api/check?ip=" + ip + "&accessKey=" + accessKey;
+
+    // Make a request and store the response
+    const response = await fetch(url);
+
+    // Decode JSON response:
+    const result = await response.json();
+
+    // Output the "code" value inside "currency" object
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+});
 module.exports = {create_shops}
