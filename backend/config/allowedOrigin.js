438e6d8623031f0e8e4451fd58a9bee9a6d7f64a
codeironside <fury25423@gmail.com>
2023-11-24 14:31:45 +0100

    updated cors origin

--- backend/config/allowedOrigin.js
+++ backend/config/allowedOrigin.js
@@ -2,7 +2,8 @@ const allowedOrigin = [
     "http://localhost:3000", 
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://universoulbarbers.com"
+    "https://universoulbarbers.com",
+    "https://barbers-ochre.vercel.app/"
   ];
   module.exports = allowedOrigin;
   
