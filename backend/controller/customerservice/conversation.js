70da1c68eaac60dcecf8ccc4e5daa26d53ba3df8
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-03-01 12:12:38 +0100

    Update conversation.js

--- backend/controller/customerservice/conversation.js
+++ backend/controller/customerservice/conversation.js
@@ -135,10 +135,41 @@ const getMessages = asynchandler(async (req, res) => {
     });
   }
 });
+const getAllMessagesForUser = asynchandler(async (req, res) => {
+  try {
+    const { id } = req.auth;
+
+    // Find all conversations where the user is either user_one or user_two
+    let conversations = await Conversation.find({
+      $or: [
+        { user_one: id },
+        { user_two: id },
+      ],
+    })
+    .populate("user_one")
+    .populate("user_two")
+    .populate("messages.sender");
+
+    if (!conversations) {
+      throw Object.assign(new Error("No messages for this user"), {
+        statusCode: 404,
+      });
+    }
+
+    res.status(200).json({ messages: conversations });
+  } catch (error) {
+    console.error("Error retrieving messages:", error);
+    throw Object.assign(new Error(`${error.message}`), {
+      statusCode: error.statusCode,
+    });
+  }
+});
+
 
 // Retrieve messages in a conversation
 
 module.exports = {
   sendMessage,
   getMessages,
+  getAllMessagesForUser
 };
